1ee9392d3c94afee553b195091a081ef7c20e085
José Gabriel <josegabrieleblingpasinato@gmail.com>
2024-09-18 22:59:48 -0300

    Tarefa completa.
    Comentários adicionados

--- codigo.py
+++ codigo.py
@@ -5,26 +5,25 @@ ou até mesmo de 100, dependendo da quantidade que o usuário requisitar.'''
 
 
 def mdc():
-    numbers = list()
-    while True:
+    numbers = list() #criação da lista de números
+    while True: #loop para o usuário inserir os números de sua preferência.
         num = int(input('Insira os números para o MDC (0 para encerrar!): '))
         if num != 0:
             numbers.append(num)
             continue
         else:
             break
-    numbers.sort(reverse=True)
-    while True:
-        resposta = numbers[0] % numbers[1]
-        if resposta != 0:
+    numbers.sort(reverse=True) #comando para ordenar a lista de números do maior ao menor
+    while True: #loop de algoritmo de euclides (dois primeiros algarismos)
+        resposta = numbers[0] % numbers[1] #operação de resto de divisão
+        if resposta != 0: #condicional de teste "se a resposta for diferente de 0" o divisor é guardado e o resto da operação se torna o divisor
             numbers[0] = numbers[1]
             numbers[1] = resposta
-            continue
-        else:
-            if len(numbers) == 2:
-                return numbers[1]
-            else:
-                numbers.pop(0)
-                continue
+            continue #volta para o começo do loop
+        else: #se o resto for = 0.
+            if len(numbers) == 2: #teste para ver se há mais números para prosseguir o algoritmo de euclides, se não houver mais, há o retorno da resposta
+                return print(f" O MDC dos números inseridos é {numbers[1]}")
+            else: #se houver mais números para realizar a operação de euclides.
+                numbers.pop(0) #apaga o primeiro elemento da lista e guarda os números que ainda faltam calcular
+                continue #volta para o loop da operação de euclides
 
-print(mdc())
